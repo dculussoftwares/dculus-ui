59918bdb93abcc1264c9ed3d5147e0b03543d849
natheeshkumar.r <natheeshkumar.r@thoughtworks.com>
2022-04-01 15:26:21 +0530

    Add theme color

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./tailwind.css";
 import Button from "./components/Button";
 import Btn from "./components/Btn";
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
+
+
 
 const App = () => {
+  useEffect(() => {
+    themeChange(false)
+    // 👆 false parameter is required for react project
+  }, [])
   return (
     <div className="App bg-[#eee] h-[100vh] flex items-center justify-center flex-col">
       <h1 className="font-bold text-4xl text-[#333]">Hi 👋, Tailwind test.</h1>
@@ -11,6 +19,13 @@ const App = () => {
         your app
       </p>
       <Button />
+      <body>
+        <button data-set-theme="" data-act-class="ACTIVECLASS">dummy </button>
+        <button data-set-theme="dark" data-act-class="ACTIVECLASS">
+          dark
+        </button>
+        <button data-set-theme="pink" data-act-class="ACTIVECLASS">pink</button>
+      </body>
 
       <Btn />
     </div>
