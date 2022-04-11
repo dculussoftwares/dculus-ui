a882daca340e17a0acb6be277dedbac51e53a57a
natheeshkumar.r <natheeshkumar.r@thoughtworks.com>
2022-04-11 15:43:31 +0530

    Fix edge on button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,22 +11,10 @@ const Button = ({ buttonType = 'primary', icon, name }: ButtonProps) => {
     const primaryBtn = buttonType == 'primary';
     const secondaryBtn = buttonType == 'secondary';
     return (
-        <div>
-            <button className={classnames("btn", 'primary', { "bg-indigo-500": primaryBtn },
-                { "bg-white": secondaryBtn }, { "hover:bg-indigo-600": primaryBtn }, { "text-white": primaryBtn },
-                { "text-black": secondaryBtn }, { "border-slate-200": secondaryBtn },
-                { "hover:border-slate-300": secondaryBtn })}>
-
-                {icon}
-                <span className={classnames("xs:block", { "ml-1": icon })}>Add view</span>
-            </button>
-
-            <button className={classnames('btn', { 'btn-primary': primaryBtn }, { "btn-secondary": secondaryBtn })}>
-                {icon}
-                <span className={classnames("xs:block", { "ml-1": icon })}> {name}</span>
-            </button>
-
-        </div>
+        <button className={classnames('btn', { 'btn-primary': primaryBtn }, { "btn-secondary": secondaryBtn })}>
+            {icon}
+            <span className={classnames("xs:block", { "ml-1": icon })}> {name}</span>
+        </button>
     );
 };
 
