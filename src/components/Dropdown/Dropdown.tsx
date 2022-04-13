cda8b8fdd41d174231d85a9b69800ae8fc70e14a
natheeshkumar.r <natheeshkumar.r@thoughtworks.com>
2022-04-13 17:56:07 +0530

    Add theme color

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -11,8 +11,39 @@ interface DropdownProps {
 
 
 const Dropdown = (props) => {
+
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: '1px dotted pink',
+      color: state.isSelected ? 'red' : 'blue',
+      padding: 20,
+    }),
+    control: () => ({
+      // none of react-select's styles are passed to <Control />
+      width: 200,
+    }),
+    singleValue: (provided, state) => {
+      const opacity = state.isDisabled ? 0.5 : 1;
+      const transition = 'opacity 300ms';
+  
+      return { ...provided, opacity, transition };
+    }
+  }
+
+
   return (
     <Select
+    theme={(theme) => ({
+      ...theme,
+      borderRadius: 0,
+      colors: {
+        ...theme.colors,
+        primary25: "var(--primary-color-border-hover)",
+        primary: "var(--primary-color)",
+      },
+    })}
       {...props}
     />
   )
