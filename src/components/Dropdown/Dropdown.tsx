7797ef597005f6216719bb7955e4ab75f1950e42
natheeshkumar.r <natheeshkumar.r@thoughtworks.com>
2022-04-13 20:17:00 +0530

    Refactor color

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -13,39 +13,102 @@ interface DropdownProps {
 const Dropdown = (props) => {
 
 
+  // const customStyles = {
+  //   option: (provided, state) => ({
+  //     ...provided,
+  //     borderBottom: '1px dotted pink',
+  //     color: state.isSelected ? 'red' : 'blue',
+  //     padding: 20,
+  //   }),
+  //   control: () => ({
+  //     // none of react-select's styles are passed to <Control />
+  //     width: 200,
+  //   }),
+  //   singleValue: (provided, state) => {
+  //     const opacity = state.isDisabled ? 0.5 : 1;
+  //     const transition = 'opacity 300ms';
+
+  //     return { ...provided, opacity, transition };
+  //   }
+  // }
+
+  // const customStyles = {
+  //   input: (provided, state) => ({
+  //     ...provided,
+  //     width: 100,
+  //     height: 20,
+  //     display: 'flex',
+  //     alignItems: 'center',
+  //   }),
+  //   singleValue: (provided, state) => ({
+  //     ...provided,
+  //     marginTop: 2,
+  //   }),
+  // };
+  const targetHeight = 32;
+
   const customStyles = {
-    option: (provided, state) => ({
+    input: (provided, state) => ({
       ...provided,
-      borderBottom: '1px dotted pink',
-      color: state.isSelected ? 'red' : 'blue',
-      padding: 20,
+      margin: 0,
+     
+      // fontSize: 14
+      // width: 100,
+      // height: 20,
+      // display: 'flex',
+      // alignItems: 'center',
     }),
-    control: () => ({
-      // none of react-select's styles are passed to <Control />
-      width: 200,
+    control: base => ({
+      ...base,
+      fontSize: 14,
+      minHeight: 'initial',
     }),
-    singleValue: (provided, state) => {
-      const opacity = state.isDisabled ? 0.5 : 1;
-      const transition = 'opacity 300ms';
-  
-      return { ...provided, opacity, transition };
-    }
-  }
-
-
+    valueContainer: base => ({
+      ...base,
+      height: `${targetHeight - 1 - 1}px`,
+      padding: '0 8px',
+    }),
+    clearIndicator: base => ({
+      ...base,
+      padding: `${(targetHeight - 20 - 1 - 1) / 2}px`,
+    }),
+    dropdownIndicator: base => ({
+      ...base,
+      padding: `${(targetHeight - 20 - 1 - 1) / 2}px`,
+    }),
+  };
+  // const customStyles = {
+  //   control: provided => ({
+  //     ...provided,
+  //     minHeight: "20px",
+  //   }),
+  //   indicatorsContainer: provided => ({
+  //     ...provided,
+  //     height: "20px"
+  //   }),
+  //   clearIndicator: provided => ({
+  //     ...provided,
+  //     padding: "5px"
+  //   }),
+  //   dropdownIndicator: provided => ({
+  //     ...provided,
+  //     padding: "5px"
+  //   })
+  // };
   return (
     <Select
-    theme={(theme) => ({
-      ...theme,
-      borderRadius: 0,
-      colors: {
-        ...theme.colors,
-        primary25: "var(--primary-color-border-hover)",
-        primary: "var(--primary-color)",
-        neutral30: "var(--primary-color)",
-        neutral20: "var(--primary-color-border)",
-      },
-    })}
+      styles={customStyles}
+      theme={(theme) => ({
+        ...theme,
+        borderRadius: 0,
+        colors: {
+          ...theme.colors,
+          primary25: "var(--primary-color-border-hover)",
+          primary: "var(--primary-color)",
+          neutral30: "var(--primary-color)",
+          neutral20: "var(--primary-color-border)",
+        },
+      })}
       {...props}
     />
   )
