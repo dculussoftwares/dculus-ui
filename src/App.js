f2b919ef1f41b356f0ce0ea0304470773e36e83f
rangasamyn@b2b.regn.net <rangasamyn@b2b.regn.net>
2022-04-20 23:17:31 +0530

    Beautify startup page

--- src/App.js
+++ src/App.js
@@ -16,34 +16,59 @@ const App = () => {
     // 👆 false parameter is required for react project
   }, [])
   return (
-    <div className="App bg-[#eee] h-[100vh] flex items-center justify-center flex-col">
-      <h1 className="font-bold text-4xl text-[#333]">Hi 👋, Tailwind test.</h1>
-      <p className="text-sm font-italic m-[3px]">
-        Now you can use <code className="bg-[#ccc] p-[3px]">[]</code> to style
-        your app
-      </p>
-      <Button primary={true}
-        label={'Button'}
-        name={'Button'} />
-      <Input />
-      <DropDown options={[
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' },
-      ]} />
-      <RadioGroup options={[
-        'Chocolate', 'Strawberry', 'Vanilla'
-      ]} />
+
+    <div className="w-1/2">
       <body>
+        <div className="flex flex-col space-y-4">
+          <Button primary={true}
+            label={'Button'}
+            name={'Button'} />
+          <Input />
+          <DropDown options={[
+            { value: 'chocolate', label: 'Chocolate' },
+            { value: 'strawberry', label: 'Strawberry' },
+            { value: 'vanilla', label: 'Vanilla' },
+          ]} />
+          <RadioGroup options={[
+            'Chocolate', 'Strawberry', 'Vanilla'
+          ]} />
+        </div>
+
         <button data-set-theme="" data-act-class="ACTIVECLASS">dummy </button>
         <button data-set-theme="dark" data-act-class="ACTIVECLASS">
           dark
         </button>
         <button data-set-theme="dragon" data-act-class="ACTIVECLASS">dragon</button>
       </body>
-
-      <Btn />
     </div>
+    // <div className="App bg-[#eee] h-[100vh] flex items-center justify-center flex-col">
+    //   <h1 className="font-bold text-4xl text-[#333]">Hi 👋, Tailwind test.</h1>
+    //   <p className="text-sm font-italic m-[3px]">
+    //     Now you can use <code className="bg-[#ccc] p-[3px]">[]</code> to style
+    //     your app
+    //   </p>
+    //   <Button primary={true}
+    //     label={'Button'}
+    //     name={'Button'} />
+    //   <Input />
+    //   <DropDown options={[
+    //     { value: 'chocolate', label: 'Chocolate' },
+    //     { value: 'strawberry', label: 'Strawberry' },
+    //     { value: 'vanilla', label: 'Vanilla' },
+    //   ]} />
+    //   <RadioGroup options={[
+    //     'Chocolate', 'Strawberry', 'Vanilla'
+    //   ]} />
+    //   <body>
+    //     <button data-set-theme="" data-act-class="ACTIVECLASS">dummy </button>
+    //     <button data-set-theme="dark" data-act-class="ACTIVECLASS">
+    //       dark
+    //     </button>
+    //     <button data-set-theme="dragon" data-act-class="ACTIVECLASS">dragon</button>
+    //   </body>
+
+    //   <Btn />
+    // </div>
   );
 };
 
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,13 +4,9 @@ interface IInputProps {
 }
 const Input = () => {
   return (
-    <div>
-      <p>ommm</p>
-      <input className="w-full text-sm focus:ring-transparent placeholder-slate-400 appearance-none px-2 py-1 outline-hidden input-v"
-        type="search"
-        placeholder="Search Anything…" />
-    </div>
-
+    <input className="w-full text-sm focus:ring-transparent placeholder-slate-400 appearance-none px-2 py-1 outline-hidden input-v"
+      type="search"
+      placeholder="Search Anything…" />
   )
 }
 
